9cf3d76b8a5bd757075c6f9b5db4e7aab40f6849
anishprasad-26 <anishprasad260105@gmail.com>
2026-09-08 13:21:02 +0530

    Added movie details page

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,13 +1,59 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./MovieDetails.css";
 
 const MovieDetails = () => {
   const { id } = useParams();
-  return(
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_API_KEY}`,
+        );
+
+        const data = await response.json();
+
+        setMovie(data);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [id]);
+
+  if (!movie) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
     <div>
-      <h1>Movie details page...</h1>
+      <div className="hero-bg">
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        />
+        <div className="hero-overlay2"></div>
+        <div className="details-hero-content">
+          <h1>{movie.title}</h1>
+
+          <div className="movie-meta">
+            <span>⭐ {movie.vote_average.toFixed(1)}</span>
+
+            <span>{movie.release_date?.split("-")[0]}</span>
+
+            <span>
+              ⏱️ {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}m
+            </span>
+          </div>
+
+          <p>{movie.overview}</p>
+        </div>
+      </div>
     </div>
   );
- 
 };
 
 export default MovieDetails;
